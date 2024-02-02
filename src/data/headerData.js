96da1efd14ceaceeaff504c62b9080987e281c18
Taran234 <ts406093@gmail.com>
2024-02-02 18:54:54 -0500

    check

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,9 @@
 import resume from '../assets/pdf/resume.pdf'
 
 export const headerData = {
-    name: 'Jane Doe',
+    name: 'portfolio template',
     title: "Web Developer",
     desciption:"There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-    image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+    image: 'https://cdn.pixabay.com/photo/2024/01/18/10/50/cormorant-8516719_1280.jpg',
     resumePdf: resume
 }
